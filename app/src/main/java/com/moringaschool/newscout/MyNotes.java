24b39e7bb3d7fa9a970d7808e57b921361fcaae5
carlos kipkoech <carlos.kipkoech@student.moringaschool.com>
2021-11-30 23:38:24 +0300

    add addnote file

--- app/src/main/java/com/moringaschool/newscout/MyNotes.java
+++ app/src/main/java/com/moringaschool/newscout/MyNotes.java
@@ -2,6 +2,7 @@ package com.moringaschool.newscout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -14,6 +15,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MyNotes extends AppCompatActivity{
+    @BindView(R.id.imageViewOpenDialog)  ImageView ButtonOpenAddNote;
 
 
 
@@ -23,6 +25,15 @@ public class MyNotes extends AppCompatActivity{
         setContentView(R.layout.activity_my_notes);
 
         ButterKnife.bind(this);
+
+        ButtonOpenAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyNotes.this,AddNoteActivity.class);
+                startActivity(intent );
+
+            }
+        });
 
 
     }
--- app/src/main/java/com/moringaschool/newscout/Note.java
+++ app/src/main/java/com/moringaschool/newscout/Note.java
@@ -1,4 +1,27 @@
 package com.moringaschool.newscout;
 
 public class Note {
+    private  String title;
+    private String description;
+
+    public Note(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
